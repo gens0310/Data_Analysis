aec405d77f350b061d0aee89f6c1d23932ddd0f5
Jin-hyuk Kim <gens0310@naver.com>
2021-08-26 16:33:43 +0900

    MySQL Practice

---
+++ 06.MySQL/projects/db_module.py
@@ -0,0 +1,25 @@
+import pymysql
+def get_monthly_data(config):
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+    sql = '''
+    SELECT DATE_FORMAT(sdate, '%m') AS `month`,
+    SUM(revenue) AS revenue, SUM(profit) AS profit
+    FROM sales_book
+    GROUP BY `month`
+    ORDER BY `month`;
+    '''
+    cur.execute(sql)
+    results = cur.fetchall()
+    cur.close()
+    conn.close()
+    return results
+def set_month(config, params):
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+    sql = '''
+    UPDATE sales SET sid=%s, sunit=%s WHERE sid=%s;
+    '''
+    cur.execute(sql, params)
+    cur.close()
+    conn.close()
