d9baf3a04cd93dcfd1e149c49ff9b6f9acdd135c
Jin-hyuk Kim <gens0310@naver.com>
2021-08-26 17:45:19 +0900

    MySQL Practice

--- 06.MySQL/projects/db_module.py
+++ 06.MySQL/projects/db_module.py
@@ -1,10 +1,12 @@
 import pymysql
+# 월별 매출/이익
 def get_monthly_data(config):
     conn = pymysql.connect(**config)
     cur = conn.cursor()
     sql = '''
     SELECT DATE_FORMAT(sdate, '%m') AS `month`,
-    SUM(revenue) AS revenue, SUM(profit) AS profit
+    SUM(revenue) AS revenue,
+    SUM(profit) AS profit
     FROM sales_book
     GROUP BY `month`
     ORDER BY `month`;
@@ -14,12 +16,70 @@ def get_monthly_data(config):
     cur.close()
     conn.close()
     return results
-def set_month(config, params):
+# 거래처별 매출/이익
+def get_data_by_company(config):
     conn = pymysql.connect(**config)
     cur = conn.cursor()
     sql = '''
-    UPDATE sales SET sid=%s, sunit=%s WHERE sid=%s;
+    SELECT scompany,
+    SUM(revenue) AS revenue,
+    SUM(profit) AS profit
+    FROM sales_book
+    GROUP BY scompany
+    ORDER BY revenue DESC;
     '''
-    cur.execute(sql, params)
+    cur.execute(sql)
+    results = cur.fetchall()
     cur.close()
     conn.close()
+    return results
+# 거래처별 판매제품/수량
+def get_products_by_company(config):
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+    sql = '''
+    SELECT scompany, pname,
+    SUM(sunit) AS unit
+    FROM sales_book
+    GROUP BY scompany, pname
+    ORDER BY scompany;
+    '''
+    cur.execute(sql)
+    results = cur.fetchall()
+    cur.close()
+    conn.close()
+    return results
+# 제품별 판매수량/매출/이익
+def get_data_by_products(config):
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+    sql = '''
+    SELECT pname,
+    SUM(sunit) AS unit,
+    SUM(revenue) AS revenue,
+    SUM(profit) AS profit
+    FROM sales_book
+    GROUP BY pid
+    ORDER BY pid;
+    '''
+    cur.execute(sql)
+    results = cur.fetchall()
+    cur.close()
+    conn.close()
+    return results
+# 카테고리별 매출/이익
+def get_data_by_category(config):
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+    sql = '''
+    SELECT pcategory,
+    SUM(revenue) AS revenue,
+    SUM(profit) AS profit
+    FROM sales_book
+    GROUP BY pcategory;
+    '''
+    cur.execute(sql)
+    results = cur.fetchall()
+    cur.close()
+    conn.close()
+    return results
